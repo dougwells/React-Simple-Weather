2f3730767e592c3d6a8dd57db004b7984e5ad2d9
dougwells <dougwells@gmail.com>
2016-06-01 08:35:48 -0600

    Promises.  One param in resolve() and reject()

--- example-promise.js
+++ example-promise.js
@@ -1,17 +1,36 @@
+//
+//
+// getTemperatureCallback('Philadelphia', function(err, temp){
+//   if(err){
+//     console.log('error', err, temp);
+//   }else{
+//     console.log('success', temp);
+//   }
+//
+// });
+//
+// function getTemperatureCallback (city, callback){
+//   console.log(city);
+//   callback(undefined, 78);
+//   callback('City not found', 82);
+//
+// }
 
 
-getTemperatureCallback('Philadelphia', function(err, temp){
-  if(err){
-    console.log('error', err);
-  }else{
-    console.log('success', temp);
-  }
 
-});
+getTempPromise ('Philadelphia').then(successFunction, errorFunction);
 
-function getTemperatureCallback (city, callback){
-  console.log(city);
-  callback(undefined, 78);
-  callback('City not found');
+function getTempPromise(location){
+  return new Promise(function(resolve, reject){
+    resolve({temp: 82, city: location});    //looks like we can only pass one param
+    reject('City not found');               //into resolve & reject
+  });
+}
 
+function successFunction(data){
+  console.log('The Temp is '+data.temp+' in ', data.city);
+}
+
+function errorFunction(message){
+  console.log(message)
 }
