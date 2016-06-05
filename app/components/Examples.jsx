edd671d1a33d221022b0bf41a023538bbf474887
dougwells <dougwells@gmail.com>
2016-06-05 09:43:36 -0600

    Changes to About Page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,9 @@
 var React = require('react');
 
-var Examples = (props)=> <h3>Examples</h3>
+var Examples = (props)=>
+  <div>
+    <h3>Examples</h3>
+    <p>Welcome to the examples page</p>
+    </div>
 
 module.exports = Examples
